f34157b7b7e91b832ebec31d3562566f864613d7
n1d3v <135556230+n1d3v@users.noreply.github.com>
2024-02-27 18:00:39 +0000

    add rewrited site

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://starixproject.lol',
+	integrations: [mdx(), sitemap(), icon()],
+});
